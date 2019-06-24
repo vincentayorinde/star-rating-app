ed3c78329d11291a83a89bb4c4cd20ac9c1c6d9a
vincentayorinde <vincentayorinde@yahoo.com>
2019-06-24 18:35:56 +0100

    initial commit

---
+++ src/components/StarRating.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Star from './Star';
+class StarRating extends Component {
+  // Initialize a 'rating' state
+  state = {
+    rating: ''
+  };
+  // Write a function that returns 5 Star components
+  fiveStar = () => {
+    let stars = [];
+    let StarRating = 5;
+    for (let i = 0; i < StarRating; i++) {
+      stars.push(
+        <Star
+          isSelected={this.state.rating > i}
+          key={i}
+          setRating={() => this.handleRating(i + 1)}
+        />
+      );
+    }
+    return stars;
+  };
+  // Write an event handler that updates the rating state.
+  handleRating = rating => {
+    this.state.rating === rating
+      ? this.setState({ rating: 0 })
+      : this.setState({ rating });
+  };
+  // Pass the function to a Star component via props
+
+  render() {
+    return (
+      <ul className='course--stars'>
+        {/* Render the Star components */}
+        {this.fiveStar()}
+      </ul>
+    );
+  }
+}
+
+export default StarRating;
